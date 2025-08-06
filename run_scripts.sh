edc5d5da87364fb2d88a05bcc4f7b486eb56edf5
rida12b <rida.boualam@gmail.com>
2025-08-06 16:55:29 +0000

    feat(deploy): Premier déploiement (sans le fichier .deb)

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Ce script sera utilisé par le Cron Job sur Render.
-# Il s'assure que la base de données a bien les bonnes tables,
-# puis lance les scripts d'extraction.
-
 echo "Initialisation de la base de données (si nécessaire)..."
 python manage.py migrate
 
 echo "Lancement du script d'extraction des prix depuis l'API..."
 python scripts/extraction_api.py
 
-echo "Lancement du script d'extraction des actualités par scraping..."
+echo "Lancement du script d'extraction des actualités depuis le FLUX RSS..."
 python scripts/extraction_news.py
 
 echo "Lancement du script d'extraction depuis la source SQL..."
